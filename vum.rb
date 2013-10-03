517916a4c4258ff3e9f3880d9d5d19155b736f0e
Amila Perera <perera.amila@gmail.com>
2013-10-03 23:30:11 +0530

    implementing vum interactive menu

--- vum.rb
+++ vum.rb
@@ -8,8 +8,11 @@ require 'colorize'
 
 class Vum
   VUM_REPOS_FILE = ENV["HOME"] + "/.vum_repos"
+  VUM_MAIN_MENU_INSTALL_WITHOUT_CHECK = 1
+  VUM_MAIN_MENU_INSTALL_WITH_CHECK = 2
+  VUM_MAIN_MENU_CHECK = 3
 
-  attr_reader :repolist
+  attr_reader :repolist, :ok_repolist, :failed_repolist, :download_ok_count, :download_failed_count
 
   def initialize
     @repolist = get_repolist_from_file # if success repolist hash is sorted by plugin name
@@ -34,14 +37,16 @@ class Vum
   end
 
   def install_plugins_with_check
-    puts "vum is now checking for repository existence"
+    puts
+    puts "Checking for repository existence"
+    puts
     check_for_repo_existence
 
     puts
-    puts "#{@ok_repolist.length}/#{@repolist.length}".bold.green +
+    puts "#{@ok_repolist.length}/#{@repolist.length}" +
           " repositories seem to be good enough for downloading" if @ok_repolist.length > 0
 
-    puts "#{@failed_repolist.length}/#{@repolist.length}".bold.red +
+    puts "#{@failed_repolist.length}/#{@repolist.length}" +
           " repositories were found to have some troubles" +
           " and vum will not use those repositories for downloading" if @failed_repolist.length > 0
 
@@ -80,7 +85,9 @@ class Vum
   end
 
   def download_plugins
+    puts
     puts "Plugins download starts"
+    puts
 
     max_repo_plugin_site_name_length = get_max_repo_line_length(@ok_repolist)
 
@@ -121,8 +128,67 @@ class Vum
     max_repo_plugin_site_name_length
   end
 
+  def self.show_vum_main_menu_with_prompt
+    puts
+    puts "  VUM (Vim bUndle Manager)"
+    puts "  ========================"
+    puts
+    puts "  " + "#{VUM_MAIN_MENU_INSTALL_WITHOUT_CHECK}" + ". Install plugins (without checking for repositories)"
+    puts "  " + "#{VUM_MAIN_MENU_INSTALL_WITH_CHECK}" + ". Install plugins after repository check"
+    puts "  " + "#{VUM_MAIN_MENU_CHECK}" + ". Check for repositories"
+    puts "  " + "q" + ". Quit"
+    puts
+    print "  Choice ? "
+    gets.chomp
+  end
+
+  def self.show_prompt
+    print "  Choice ? "
+    gets.chomp
+  end
+
 end
 
 # main
+input = Vum.show_vum_main_menu_with_prompt
+
 vum = Vum.new
-vum.install_plugins_without_check
+
+begin
+  is_good_choice = true
+  exit if input == "q" || input == "Q"
+
+  case input.to_i
+  when Vum::VUM_MAIN_MENU_INSTALL_WITHOUT_CHECK
+    puts
+    puts "Install plugins without checking for repository existence"
+    puts "========================================================="
+    vum.install_plugins_without_check
+    puts
+    puts "  #{vum.download_ok_count}/#{vum.repolist.length} plugins were downloaded successfully" if vum.download_ok_count > 0
+    puts "  #{vum.download_failed_count}/#{vum.repolist.length} plugins failed during downloading for some reason" if vum.download_failed_count > 0
+    puts
+
+  when Vum::VUM_MAIN_MENU_INSTALL_WITH_CHECK
+    puts
+    puts "Install plugins after checking for repository existence"
+    puts "======================================================="
+    vum.install_plugins_with_check
+    puts
+    puts "  #{vum.download_ok_count}/#{vum.repolist.length} plugins were downloaded successfully" if vum.download_ok_count > 0
+    puts "  #{vum.download_failed_count}/#{vum.repolist.length} plugins failed during downloading for some reason" if vum.download_failed_count > 0
+    puts
+
+  when Vum::VUM_MAIN_MENU_CHECK
+    puts
+    puts "Checking for repository existence"
+    puts "================================="
+    vum.check_for_repo_existence
+
+  else
+    is_good_choice = false
+
+    puts "  Wrong choice..."
+    input = Vum.show_prompt
+  end
+end while not is_good_choice
